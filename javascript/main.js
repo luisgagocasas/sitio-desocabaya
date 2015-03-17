f24205e9dd6b1e9a59da41098a7786bd0ee7efea
Luis Gago Casas <luisgago@lagc-peru.com>
2015-03-17 18:53:00 -0500

    timestamp 2015/05/1

--- javascript/main.js
+++ javascript/main.js
@@ -15,7 +15,7 @@ $(document).ready( function(){
     var $countdown = $('.countdown');
     $countdown.scountdown({
         // timestampgenerator.com
-        timestamp: parseInt(1427569200000, 10),
+        timestamp: parseInt(1430506800000, 10),
         callback: function (d, h, m, s) {
             $(".dias", $countdown).text(d);
             $(".horas", $countdown).text(h);
